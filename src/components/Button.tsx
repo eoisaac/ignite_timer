8fe226a9949dd7ee7f49863cebaa461824e99088
Isaac <eoisaacc@gmail.com>
2022-07-20 21:53:49 -0300

    build: add styled components and create default theme

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonContainer, ButtonVariant } from './Button.styles';
+
+interface Props {
+	variant: ButtonVariant;
+}
+
+export const Button = ({ variant='primary' }: Props) => {
+	return (
+		<ButtonContainer variant={variant}>
+			Enviar
+		</ButtonContainer>
+	);
+}
